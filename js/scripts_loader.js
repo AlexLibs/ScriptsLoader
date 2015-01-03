f52b0d435d75fff9696aae58fba1943f0b0fd78e
AlexLibs <012alex012@gmail.com>
2015-01-04 01:20:01 +0200

    Create scripts_loader.js

---
+++ js/scripts_loader.js
@@ -0,0 +1,71 @@
+function ScriptsLoader(dependencyForest, callback) {
+    var head = document.getElementsByTagName("head")[0] || document.documentElement;
+    var processed = 0;
+    var toBeProcessed = 0;
+    var scriptsLoaded = {}; // to check uniqueness
+    function loadOne(src, innerCallback) {
+        toBeProcessed++;
+        if(scriptsLoaded[src]){
+            onDone();
+            return;
+        }
+        scriptsLoaded[src] = true;
+        // console.log('Loading - ' + src);
+        var done = false;
+        var script = document.createElement('script');
+        script.onload = script.onreadystatechange = function() {
+            if (!done && (!this.readyState || this.readyState === "loaded" || this.readyState === "complete")) {
+                done = true;
+                console.log('Loaded - ' + src);
+
+                // Handle memory leak in IE
+                script.onload = script.onreadystatechange = null;
+                if (head && script.parentNode) {
+                    head.removeChild(script);
+                }
+                onDone();
+            }
+        };
+        script.src = src;
+        script.type = 'text/javascript';
+        head.appendChild(script);
+
+        function onDone() {
+            if (typeof(innerCallback) == 'function') {
+                innerCallback();
+            };
+            processed++;
+            if (processed == toBeProcessed) {
+                callback(scriptsLoaded);
+            }
+        }
+    }
+    var processors = {
+        '[object String]': loadOne,
+        '[object Array]': function(array) {
+            for (var key in array) {
+                load(array[key]);
+            }
+        },
+        '[object Object]': function(object) {
+            for (var key in object) {
+                (function(child) {
+                    loadOne(key, function() {
+                        load(child);
+                    });
+                })(object[key]);
+            }
+        }
+    };
+
+    function load(subForest) {
+        var processor = processors[toString.call(subForest)];
+        if (!processor) {
+            throw 'Malformated dependencyForest'
+        }
+        else {
+            processor(subForest);
+        }
+    }
+    load(dependencyForest);
+};
